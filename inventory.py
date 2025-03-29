228b498a6f7c12deaeb4d52af87765e7e2d46cb0
OA <>
2025-03-29 16:38:55 -0400

    initial commit

---
+++ inventory.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+import os
+import json
+
+homelab_ip = os.getenv("HOMELAB_IP")
+ansible_user = os.getenv("ANSIBLE_USER")
+
+inventory = {
+    "homelab": {
+        "hosts": [homelab_ip],
+        "vars": {
+            "ansible_user": ansible_user
+        }
+    }
+}
+
+print(json.dumps(inventory))
+
